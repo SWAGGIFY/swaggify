0aba366f53da3bbf0c54a3795cad5ae1c8071240
Knowledge <knowhowkwaramba@gmail.com>
2020-02-19 09:14:24 +0200

    New folder shared

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -7,9 +7,23 @@ router.get('/admin-dashboard', ensureAuthentication, function(req, res){
     //res.send(req.user)
    res.render('./admin/admin-dashboard',{
        //user:req.user,
-       layout:"../layouts"
+       layout:"../layouts/authenticated.handlebars"
    });
 });
+
+router.get('/admin-view-users', ensureAuthentication, function(req, res){
+  //res.send(req.user)
+  User.find({},(err, users)=>{
+    //var user = [users];
+    if(err) throw err;
+      //res.send(users);
+    res.render('./admin/view-users',{
+      users:users,
+      layout:"../layouts/authenticated.handlebars"
+    });
+  });
+});
+
 
 //Ensure the user is authentecated
 function ensureAuthentication(req, res, next){
