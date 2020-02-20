eddcb1c249bc03b71a5fcdd5bc326b00392d5388
Knowledge <knowhowkwaramba@gmail.com>
2020-02-20 16:40:09 +0200

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,6 @@
 module.exports ={
     google:{
-      clientID:'211111895005-2flkth2u09rpcccvahr5og2rc2vimaqc.apps.googleusercontent.com',
-      clientSecret:'ad-xzBhT8PYmSLJqDiu3fdZB'
+      
     },
     facebook:{
       clientID:'My_secret',
